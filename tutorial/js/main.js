2f379e50925b9bee8ec9808a5a6f49fc706bd6b6
Dmytro Bychev (Fillnati) <dmitriybychev2012@gmail.com>
2022-10-20 00:13:22 +0300

    upd

--- tutorial/js/main.js
+++ tutorial/js/main.js
@@ -1,2 +1,7 @@
 console.log (3 ** 5);
 console.log (-8 /-4);
+console.log ("Привіт, як справи?");
+var document = Document;
+// document.querySelector('.container');
+// console.log (document.querySelector('.container'));
+console.log (document.querySelector('.page'));
